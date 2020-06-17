e8aee3f8e534c6a43bfe149214db7cdf6e8bf129
windking0301 <33643671+windking0301@users.noreply.github.com>
2020-06-17 08:39:12 -0700

    API_URL update

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,9 @@ module.exports = {
 		public: '127.0.0.1:8080',
 		proxy: {
 			'/': {
-				target: process.env.API_URL ? process.env.API_URL : 'https://demo.directus.io/',
+				target: process.env.API_URL
+					? 'https://worksdomain.nl/public/'
+					: 'https://demo.directus.io/',
 				changeOrigin: true
 			}
 		}
